968cb0c6c6b0093636fd64dfeecfad87df25b648
Pedro Guridi <pguridi@onapsis.com>
2015-09-16 12:10:14 -0300

    cleanup sandbox.py

--- run_sandbox.py
+++ run_sandbox.py
@@ -1,24 +1,5 @@
 #! /usr/bin/env python
 
-"""Interacts with a PyPy subprocess translated with --sandbox.
-
-Usage:
-    pypy_interact.py [options] <executable> <args...>
-
-Options:
-    --tmp=DIR     the real directory that corresponds to the virtual /tmp,
-                  which is the virtual current dir (always read-only for now)
-    --heapsize=N  limit memory usage to N bytes, or kilo- mega- giga-bytes
-                  with the 'k', 'm' or 'g' suffix respectively.
-    --timeout=N   limit execution time to N (real-time) seconds.
-    --log=FILE    log all user input into the FILE.
-    --verbose     log all proxied system calls.
-
-Note that you can get readline-like behavior with a tool like 'ledit',
-provided you use enough -u options:
-
-    ledit python -u pypy_interact.py pypy-c-sandbox -u
-"""
 import sys
 import os
 
@@ -28,7 +9,6 @@ sys.path.insert(0, os.path.realpath(PYPY_PATH))
 
 from rpython.translator.sandbox.sandlib import SimpleIOSandboxedProc
 from rpython.translator.sandbox.sandlib import VirtualizedSandboxedProc
-#from rpython.translator.sandbox.sandlib import VirtualizedSocketProc
 from rpython.translator.sandbox.vfs import Dir, RealDir, RealFile
 import pypy
 LIB_ROOT = os.path.dirname(os.path.dirname(pypy.__file__))
@@ -50,39 +30,6 @@ def copy_basebot(bot_script, bot_cookie):
         with open(os.path.join(bot_dir, "basebot.py"), "w+") as fw:
             fw.write(base_content)
 
-
-# def create_sandbox_dir(bot_script, bot_cookie):
-#     sandbox_dir = os.path.join(TMP_DIR, bot_cookie)
-#     if not os.path.exists(sandbox_dir):
-#         os.mkdir(sandbox_dir)
-#
-#     with open(bot_script, "r") as f:
-#         bot_content = f.read()
-#         bot_content = "BOT_COOKIE='"+bot_cookie + "\n" + bot_content
-#         with open(os.path.join(sandbox_dir, "bot.py"), "w+") as f:
-#             f.write(bot_content)
-#
-#     return sandbox_dir
-#     #shutil.copy(bot_script, os.path.join(sandbox_dir, "bot.py"))
-
-
-# class BotHandler(object):
-#
-#     def __init__(self, path):
-#         self.bot_id = uuid.uuid4().hex
-#         self.queue = Queue()
-#         self.path = path
-#         fdir, fname = os.path.split(path)
-#         self.fname = fname
-#         self.sandbox_dir = None
-#
-#     def create_sandbox_dir(self):
-#         self.sandbox_dir = os.path.join(TMP_DIR, self.bot_id)
-#         os.mkdir(self.sandbox_dir)
-#         shutil.copy(self.path, os.path.join(self.sandbox_dir, "bot.py"))
-#         with open(os.path.join(self.sandbox_dir, "bot_id.py"), "w+") as f:
-#             f.write("id='"+self.bot_id+"'")
-        
 
 class PyPySandboxedProc(VirtualizedSandboxedProc, SimpleIOSandboxedProc):
     argv0 = '/bin/pypy-c'
