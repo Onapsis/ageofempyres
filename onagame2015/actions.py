8db3937a77c6a2485a9378f9d3ac0a3738f8be0e
Mariano Anaya <manaya@onapsis.com>
2015-10-29 10:44:20 -0300

    more attack actions.

--- onagame2015/actions.py
+++ onagame2015/actions.py
@@ -1,5 +1,11 @@
+import random
 from onagame2015.validations import coord_in_arena
 from onagame2015.engine import Coordinate
+
+
+def toss_dice(number_of_dice):
+    for _ in range(number_of_dice):
+        yield random.randint(1, 6)
 
 
 class BaseBotAction(object):
@@ -33,10 +39,36 @@ class AttackAction(BaseBotAction):
         according to the result.
         """
         # TODO: When a soldier attack, the enemy must be front of him and it must be from other team
+        attacker_coord = action['from']
+        defender_coord = action['to']
         self._run_attack_validations(
             arena=arena,
-            tail_from=action['from'],
-            tail_to=action['to'])
+            tail_from=attacker_coord,
+            tail_to=defender_coord,
+        )
+        attacker_tail = arena[attacker_coord.x][attacker_coord.y]
+        defender_tail = arena[defender_coord.x][defender_coord.y]
+        attack_result = self._launch_attack(
+            attacker_tail=attacker_tail,
+            defender_tail=defender_tail,
+        )
+
+    def _launch_attack(self, attacker_tail, defender_tail):
+        """Run the attack on the tails, by using the units in each one
+        @return: dict indicating how many unit loses every team
+        """
+        attacker_dice = len(attacker_dice.items)
+        defender_dice = len(defender_dice.items)
+        play = lambda n_dice: sorted(toss_dice(n_dice), reverse=True)
+        partial_result = dict(attacker_loses=0, defender_loses=0)
+        for attacker, defender in zip(play(attacker_dice), play(defender_dice)):
+            if attacker <= defender:
+                partial_result['attacker_loses'] += 1
+            else:
+                partial_result['defender_loses'] += 1
+        return partial_result
+
+
 
     def _run_attack_validations(self, arena, tail_from, tail_to):
         """Run a series of validations to assess if is possible to perform an
@@ -44,8 +76,8 @@ class AttackAction(BaseBotAction):
         self._tails_in_arena(
             tails=(Coordinate(*point) for point in (tail_to, tail_from)),
             arena=arena)
-        self._contiguous_tails(tail_from, tail_to)
-        self._oposite_bands(tail_from, tail_to)
+        self._contiguous_tails(tail_from=tail_from, tail_to=tail_to)
+        self._oposite_bands(tail_from=tail_from, tail_to=tail_to)
 
     def _tails_in_arena(self, tails, arena):
         if not all(coord_in_arena(t, arena) for t in tails):
