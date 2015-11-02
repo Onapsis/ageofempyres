c4ff7a8b967aea0521961dc9e01c857ee0cb27a2
Sebastian Aguirre Brachi <saguirreb@onapsis.com>
2015-11-02 17:08:47 -0300

    WIP changes

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,20 +6,43 @@ from onagame2015.lib import Coordinate
 from onagame2015.units import AttackUnit
 from onagame2015.arena import ArenaGrid, TileContainer
 
-
-def test_attack_unit_move_invalid_input(random_arena):
-    attack_unit = AttackUnit(Coordinate(0, 0), 1, random_arena)
-
-    result = attack_unit.move('UP')
-
-    assert result.get('error') and 'invalid' in result.get('error')
+VALID_MOVES = (Coordinate(1, 0), Coordinate(-1, 0),
+               Coordinate(0, 1), Coordinate(0, -1),
+                )
 
 
-def test_attack_unit_move_coord_not_in_arena(random_arena):
+INVALID_MOVES_FROM_00 = (Coordinate(-1, 0), Coordinate(0, -1))
+
+INVALID_INPUTS = ('UP', 2334, 0.343)
+
+
+@pytest.mark.parametrize('invalid_input', INVALID_INPUTS)
+def test_attack_unit_move_invalid_input(random_arena, invalid_input):
     initial_coordinate = Coordinate(0, 0)
     attack_unit = AttackUnit(initial_coordinate, 1, random_arena)
 
-    result = attack_unit.move((randint(101, 120), randint(101, 120)))
+    result = attack_unit.move(invalid_input)
 
     assert result.get('error') and 'invalid' in result.get('error')
     assert attack_unit.coordinate == initial_coordinate
+
+
+@pytest.mark.parametrize('invalid_move', INVALID_MOVES_FROM_00 + (999999, 123))
+def test_attack_unit_move_out_of_arena(random_arena, invalid_move):
+    initial_coordinate = Coordinate(0, 0)
+    attack_unit = AttackUnit(initial_coordinate, 1, random_arena)
+
+    result = attack_unit.move((99999, 99999))
+
+    assert result.get('error') and 'invalid' in result.get('error')
+    assert attack_unit.coordinate == initial_coordinate
+
+
+@pytest.mark.parametrize('valid_move', VALID_MOVES)
+def test_attack_unit_move(random_arena, valid_move):
+    initial_coordinate = Coordinate(1, 1)
+    attack_unit = AttackUnit(initial_coordinate, 1, random_arena)
+
+    result = attack_unit.move(valid_move)
+
+    assert not result.get('error')
