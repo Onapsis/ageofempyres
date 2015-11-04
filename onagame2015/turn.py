cf214b2433845173b78faaf71c566ea1198742cc
Mariano Anaya <manaya@onapsis.com>
2015-11-04 10:33:16 -0300

    Delete programming in GameTurn
    Remove unreached, deprecated code.

--- onagame2015/turn.py
+++ onagame2015/turn.py
@@ -1,5 +1,4 @@
 from itertools import groupby
-from collections import defaultdict
 
 
 class GameTurn(object):
@@ -9,9 +8,6 @@ class GameTurn(object):
     """
     def __init__(self, arena):
         self.arena = arena
-        self._units_in = defaultdict(int)
-        self._attacks = defaultdict(dict)
-        self._transitions = {}
         self.trace = []
         self.history = []
 
@@ -20,16 +16,6 @@ class GameTurn(object):
         the temporary status of it."""
         if not bot_response.get('error'):
             self.history.append(bot_response)
-
-    def _update_move(self, bot_response):
-        """Update a movement made by the bot. Keep track of how many units
-        entered and left each coordinate in the arena.
-        """
-        unique_transition = (bot_response['from'], bot_response['to'])
-        origin, end = unique_transition
-        self._units_in[origin] = self.arena.number_of_units_in_tile(origin) - 1
-        self._units_in[end] = self.arena.number_of_units_in_tile(end) + 1
-        self._transitions[unique_transition] = 1
 
     def summarize_moves(self, actions):
         key_func = lambda x: (x['player'], x['from'], x['to'])
@@ -94,7 +80,6 @@ class GameTurn(object):
                 "player": attack['attacker_player'],
                 "from": {
                     "tile": {"x": att_cord.longitude, "y": att_cord.latitude},
-                    # "units": attack['attacker_units'] + attack['attacker_loses'],
                     "dice": attack['attacker_dice'],
                     "remaining_units": attack['attacker_units'],
                     "lost_units": attack['attacker_loses']
@@ -102,21 +87,15 @@ class GameTurn(object):
                 "to": {
                     "player": attack['defender_player'],
                     "tile": {"x": def_cord.longitude, "y": def_cord.latitude},
-                    # "units": attack['defender_units'] + attack['defender_loses'],
                     "dice": attack['defender_dice'],
                     "remaining_units": attack['defender_units'],
                     "lost_units": attack['defender_loses']
                 },
             })
 
-    def _update_attack(self, bot_response):
-        pass
-
     def summarize_actions(self):
         for key, actions in groupby(self.history, key=lambda x: x['action_type']):
-            print 'Key:', key
             actions = list(actions)
-            print 'Actions:', actions
             if key == 'MOVE':
                 self.summarize_moves(actions)
             elif key == 'ATTACK':
